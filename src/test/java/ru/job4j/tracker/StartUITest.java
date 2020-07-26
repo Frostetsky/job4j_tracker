b96651d80114d900df3b687d0660671453d20062
Frostetsky <nesterenko-nikita@bk.ru>
2020-07-27 01:56:34 +0300

    1.1. Тесты на ValidateInput. [#292475]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -6,6 +6,27 @@ import static org.hamcrest.Matchers.nullValue;
 import static org.junit.Assert.assertThat;
 
 public class StartUITest {
+    @Test
+    public void whenInvalidExit() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] { "2", "0" }
+        );
+        Tracker tracker = new Tracker();
+        UserAction[] actions = {
+                new ExitProgramAction(out)
+        };
+        new StartUI(out).init(in, tracker, actions);
+        assertThat(out.toString(), is(
+                String.format(
+                        "Menu.%n"
+                                + "0. ==== Exit Program ====%n"
+                                + "Wrong input, you can select: 0 .. 0%n"
+                                + "Menu.%n"
+                                + "0. ==== Exit Program ====%n"
+                )
+        ));
+    }
 
     @Test
     public void whenShowAllItems() {

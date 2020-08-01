da981ffc558114035f57fcfb56d22c8766ae3d08
Frostetsky <nesterenko-nikita@bk.ru>
2020-08-02 02:22:51 +0300

    Update

--- src/main/java/ru/job4j/tracker/oop/Ball.java
+++
@@ -1,7 +0,0 @@
-package ru.job4j.tracker.oop;
-
-public class Ball {
-    public void tryEat(Ball ball) {
-
-    }
-}
--- src/main/java/ru/job4j/tracker/oop/Wolf.java
+++
@@ -1,7 +0,0 @@
-package ru.job4j.tracker.oop;
-
-public class Wolf {
-    public void tryEat(Hare hare) {
-
-    }
-}

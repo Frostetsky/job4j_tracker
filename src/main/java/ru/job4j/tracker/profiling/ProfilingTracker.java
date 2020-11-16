00a62da54dadffbad696127e3ccf96054e42cf0d
Frostetsky <nesterenko-nikita@bk.ru>
2020-11-16 08:55:49 +0300

    Профилирование. [#343070]

---
+++ src/main/java/ru/job4j/tracker/profiling/ProfilingTracker.java
@@ -0,0 +1,19 @@
+package ru.job4j.tracker.profiling;
+
+import ru.job4j.tracker.Item;
+import ru.job4j.tracker.MemTracker;
+import ru.job4j.tracker.Store;
+
+public class ProfilingTracker {
+    public static void main(String[] args) throws InterruptedException {
+        Store store = new MemTracker();
+        for (int i = 0; i < 2000; i++) {
+            store.add(new Item(String.valueOf(i), "Item" + i));
+        }
+        System.out.println("Start");
+        for (int i = 2001; i < 100000000; i++) {
+            store.add(new Item(String.valueOf(i), "Item" + i));
+            Thread.sleep(10);
+        }
+    }
+}
